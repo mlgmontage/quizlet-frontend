77c77c44286257d5bdd4d077949cad7fd2c779da
mlgmontage <kettleofrassel1@gmail.com>
2020-11-19 13:35:38 +0500

    hard codes data: questions

---
+++ src/data/questions.js
@@ -0,0 +1,24 @@
+// Data modeling
+
+export default [
+  {
+    question:
+      "In the UK, the abbreviation NHS stands for National 'what' Service?",
+    answers: [
+      { answer: "Humanity", isCorrect: false },
+      { answer: "Health", isCorrect: true },
+      { answer: "Honour", isCorrect: false },
+      { answer: "Household", isCorrect: false },
+    ],
+  },
+  {
+    question:
+      "Which Disney character famously leaves a glass slipper behind at a royal ball?",
+    answers: [
+      { answer: "Pocahontas", isCorrect: false },
+      { answer: "Sleeping Beauty", isCorrect: false },
+      { answer: "Cinderella", isCorrect: true },
+      { answer: "Elsa", isCorrect: false },
+    ],
+  },
+];
