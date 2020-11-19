16156edda1b5e71be5bd647f88658c0f86affe4d
mlgmontage <kettleofrassel1@gmail.com>
2020-11-19 12:55:43 +0500

    class component & header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div>
-      <header>Quizlet</header>
-    </div>
-  );
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <header className="container my-4">
+          <h1 className="text-center">
+            <i className="fa fa-tree"></i>
+          </h1>
+          <h1 className="text-center">Quizlet</h1>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
